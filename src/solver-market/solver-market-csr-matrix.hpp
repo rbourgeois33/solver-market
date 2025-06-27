b67ab8c8ed878f4b2fd92a5d391c86ef452eb7a4
Remi Bourgeois <Remi.Bourgeois@cea.fr>
2025-06-27 11:05:12 +0200

    remove static assert for testing

--- src/solver-market/solver-market-csr-matrix.hpp
+++ src/solver-market/solver-market-csr-matrix.hpp
@@ -9,7 +9,10 @@
 
 using Device = Kokkos::DefaultExecutionSpace;
 using Host = Kokkos::DefaultHostExecutionSpace;
+
+#ifndef GTEST_
 static_assert(!std::is_same<Device, Host>::value, "Error: Device and Host execution spaces must be different. You build Kokkos from trilinos without GPU option !");
+#endif
 
 template<typename _TYPE_>
 using HostView=Kokkos::View<_TYPE_*, Host>;
