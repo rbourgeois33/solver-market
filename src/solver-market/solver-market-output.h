cb0babed178ce90e72646fb02a123dd865eb88e1
Remi Bourgeois <Remi.Bourgeois@cea.fr>
2025-07-02 10:27:49 +0200

    cleaner output

--- src/solver-market/solver-market-output.h
+++ src/solver-market/solver-market-output.h
@@ -12,38 +12,26 @@ void SolverMarketOutput(const std::chrono::milliseconds& SolverMarketSetupTime,
     auto out_solve = std::chrono::duration_cast<std::chrono::duration<double>>(SolverMarketSolveTime);
     auto out_setup = std::chrono::duration_cast<std::chrono::duration<double>>(SolverMarketSetupTime);
 
-    std::cout << "\\---- Solver market output ----/\n";
+    std::cout << "\n \\---- Solver Market output ----/\n\n";
     std::cout << "input: ";
+    std::ostringstream input;
     for (int i = 0; i < argc; i++) {
         std::cout << argv[i] << " ";
+        input << argv[i] << " ";
     }
+
     std::cout << "\n";
     std::cout << "Success: " << success << "\n";
-    std::cout << "Setup time: " << std::setprecision(5) << out_setup.count() << " s\n";
-    std::cout << "Solve time: " << std::setprecision(5) << out_solve.count() * 1000 << " ms\n";
+    std::cout << "Setup time: " << std::setprecision(6) << out_setup.count() << " s\n";
+    std::cout << "Solve time: " << std::setprecision(6) << out_solve.count() * 1000 << " ms\n\n";
 
-    // --- Extract just the XML filename
-    std::string xmlFileName = "unknown.xml";
-    for (int i = 0; i < argc; ++i) {
-        std::string arg = argv[i];
-        auto pos = arg.find("--xml=");
-        if (pos != std::string::npos) {
-            std::string fullPath = arg.substr(pos + 6); // Skip "--xml="
-            size_t lastSlash = fullPath.find_last_of("/\\");
-            if (lastSlash != std::string::npos) {
-                xmlFileName = fullPath.substr(lastSlash + 1);
-            } else {
-                xmlFileName = fullPath;
-            }
-            break;
-        }
-    }
+    std::cout << "\n \\-----------------------------/\n";
 
     // --- Write to file (append mode)
     std::ofstream outFile("solver_output.log", std::ios::app);
     if (outFile.is_open()) {
-        outFile << xmlFileName << " "
-                << std::fixed << std::setprecision(5)
+        outFile << input.str() << " "
+                << std::fixed << std::setprecision(6)
                 << success<< " "
                 << out_setup.count() << " "
                 << out_solve.count() * 1000 << "\n";
